63a33924340de8b09374e99a9e1c774eb4873f55
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-01 00:10:36 +0500

    query and route params in node

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-const express = require('express')
-const app = express()
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// app.get("/", (req,res)=>{
+//     res.send("Hello this is home page")
+// })
+
+// app.get("/about", (req,res)=>{
+//     res.send("Hello this is about page")
+// })
+
+// app.get("/contact", (req,res)=>{
+//     res.send("Hello this is contact page")
+// })
+
+// app.get("/products/:productid", (req, res)=>{
+//      const id = req.params.productid;
+//     res.send(`Hello this is Product ${id} page`)
+//  })
+
+// query paramters
+
+// app.get("/search", (req, res)=>{
+//      console.log(req.query);
+//      res.send("Hello this is search page")
+
+// }
+
+// )
+
+// --------------------------------------------------
+ 
+ app.get("/search", (req, res)=>{
+     const name = req.query.name;
+     const city = req.query.city;
+     res.send(`Hello ${name} from ${city}`)
+
+}
+
+)
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
 })
+
+
